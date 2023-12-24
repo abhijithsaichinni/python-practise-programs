a1a62126f3d65b117d207842729ce074d6e75ff0
Abhijith Sai Chinni <99478457+abhijithsaichinni@users.noreply.github.com>
2023-12-24 13:17:30 +0530

    Create muggle-game.py

---
+++ muggle-game.py
@@ -0,0 +1,19 @@
+#
+
+print(
+"""
++================================+
+| Welcome to my game, muggle!    |
+| Enter an integer number        |
+| and guess what number I've     |
+| picked for you.                |
+| So, what is the secret number? |
++================================+
+""")
+
+number=int(input("Guess the number:"))
+
+while number!= secret_number:
+    print("Ha ha! You're stuck in my loop!")
+    number=int(input("Guess the number again:"))
+print("Well done, muggle! You are free now.")
